0b77ab0420ee40574095399979a50e84fa50ee45
wonjonghong <wonhong1996@naver.com>
2025-05-16 17:57:53 +0900

    feat: 각 페이지로 들어가서 작업할 수 있도록 4가지 버튼 추가(추후 삭제예정)

--- lib/presentation/pages/splash_page.dart
+++ lib/presentation/pages/splash_page.dart
@@ -1,1 +1,18 @@
 // 앱 시작 시 스플래시 화면 (로딩 후 PostListPage로 이동)
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash Page'),
+      ),
+      body: const Center(
+        child: Text('This is the Splash Page'),
+      ),
+    );
+  }
+}
