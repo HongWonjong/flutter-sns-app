b7a0f4afdc88caac3c2acbfefffb47a6a4874757
wonjonghong <wonhong1996@naver.com>
2025-05-19 09:46:24 +0900

    feat: datasources 작성

--- lib/data/datasources/comment_remote_datasource.dart
+++ lib/data/datasources/comment_remote_datasource.dart
@@ -1,1 +1,26 @@
-// Firestore에서 댓글 데이터 가져오기/작성
+/// Firestore에서 댓글 데이터 가져오기/작성
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_sns_app/data/dtos/comment_dto.dart';
+
+class CommentRemoteDataSource {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<List<CommentDto>> getComments(String postId) async {
+    final snapshot = await _firestore
+        .collection('posts')
+        .doc(postId)
+        .collection('comments')
+        .orderBy('createdAt')
+        .get();
+    return snapshot.docs.map((doc) => CommentDto.fromFirestore(doc)).toList();
+  }
+
+  Future<void> createComment(String postId, CommentDto comment) async {
+    await _firestore
+        .collection('posts')
+        .doc(postId)
+        .collection('comments')
+        .doc(comment.commentId)
+        .set(comment.toFirestore());
+  }
+}
--- lib/data/datasources/storage_datasource.dart
+++ lib/data/datasources/storage_datasource.dart
@@ -1,2 +1,15 @@
+/// Firebase Storage에서 이미지 업로드/URL 가져오기 (검사 후 업로드)
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:uuid/uuid.dart';
 
-// Firebase Storage에서 이미지 업로드/URL 가져오기 (검사 후 업로드)
+class StorageDataSource {
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+
+  Future<String> uploadImage(String postId, File image) async {
+    final fileName = Uuid().v4();
+    final ref = _storage.ref().child('images').child(postId).child('$fileName.jpg');
+    await ref.putFile(image);
+    return await ref.getDownloadURL();
+  }
+}
