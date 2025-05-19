3b613c38487e820647ae8ed27129e62be50486a5
wonjonghong <wonhong1996@naver.com>
2025-05-19 10:23:06 +0900

    feat: usecases 작성

--- lib/domain/usecases/create_comment_usecase.dart
+++ lib/domain/usecases/create_comment_usecase.dart
@@ -1,2 +1,13 @@
+/// 댓글 작성 유스케이스
+import 'package:flutter_sns_app/domain/entities/comment.dart';
+import 'package:flutter_sns_app/domain/repositories/comment_repository.dart';
 
-// 댓글 작성 유스케이스
+class CreateCommentUseCase {
+  final CommentRepository _repository;
+
+  CreateCommentUseCase(this._repository);
+
+  Future<void> execute(String postId, Comment comment) {
+    return _repository.createComment(postId, comment);
+  }
+}
--- lib/domain/usecases/create_post_usecase.dart
+++ lib/domain/usecases/create_post_usecase.dart
@@ -1,1 +1,13 @@
-// 게시물 작성 유스케이스 (이미지 검사 후 업로드)
+/// 게시물 작성 유스케이스 (이미지 검사 후 업로드 로직 필요)
+import 'package:flutter_sns_app/domain/entities/post.dart';
+import 'package:flutter_sns_app/domain/repositories/post_repository.dart';
+
+class CreatePostUseCase {
+  final PostRepository _repository;
+
+  CreatePostUseCase(this._repository);
+
+  Future<void> execute(Post post) {
+    return _repository.createPost(post);
+  }
+}
